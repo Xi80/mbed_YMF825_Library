a7c31b4cb48a1d853561af68e10a531dbbe51e90
Luna Tsukiyono <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-06-18 01:58:37 +0900

    readmeは途中まで

--- README.md
+++ README.md
@@ -1,1 +1,106 @@
-mbed_YMF825_Library
+# mbed YMF825 Library
+
+Copyright(c) 2020 Xi80
+
+
+
+## 概要
+
+YMF825のmbed用のライブラリがないので自分用に作りました
+
+
+
+## 使い方
+
+下のサンプルコードを参考にしてください
+
+## 対応機能
+
+- ノートオン
+- ノートオフ
+- 音色データの送信
+- ピッチベンド
+
+## サンプルコード
+
+```cpp
+#include <mbed.h>
+#include "midi/midi.h"
+#include "ymf825/ymf825.h"
+
+struct ymf825::channel channels[16];
+uint8_t midiChannels[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+uint8_t toneNumbers[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+volatile unsigned short pitchBends[16]= {8192,8192,8192,8192,8192,8192,8192,8192,8192,8192,8192,8192,8192,8192,8192,8192};
+
+midi MIDI(USBTX,USBRX);
+ymf825 YMF825(D11,D12,D13,D10,D9);
+
+void noteOff(int ch,int note);
+
+void noteOn(int ch,int note,int vel){
+  for(int i = 0;i < 16;i++){
+    if(channels[i].isUsed == false){
+      channels[i].isUsed = true;
+      channels[i].noteNumber = note;
+      midiChannels[i] = ch;
+      YMF825.noteOn(i,note,vel,toneNumbers[i],pitchBends[ch]);
+      return;
+    }
+  }
+}
+
+void noteOff(int ch,int note){
+  for(int i = 0;i < 16;i++){
+    if(channels[i].isUsed == true && channels[i].noteNumber == note && midiChannels[i] == ch){
+      YMF825.noteOff(i);
+      channels[i].isUsed = false;
+      return;
+    }
+  }
+}
+
+void controlChange(int ch,int num,int val){
+  if(num >= 51 && num <=66){
+    
+    YMF825.setToneListFromGM(num - 51,val);
+  } else if(num >= 101 && num <= 116){
+    toneNumbers[num-101] = val;
+  } else if(num == 20){
+    YMF825.sendTone();
+  }
+}
+
+void programChange(int ch,int num){
+  return;
+}
+
+void reset(void){
+  return;
+}
+
+void pitchBend(int ch,unsigned short pb){
+  pitchBends[ch] = pb;
+  for(int i = 0;i < 16;i++){
+    if(channels[i].isUsed == true && midiChannels[i] == ch){
+      YMF825.pitchBend(i,pb);
+    }
+  }
+}
+
+
+int main() {
+  MIDI.setCallbackNoteOn(noteOn);
+  MIDI.setCallbackNoteOff(noteOff);
+  MIDI.setCallbackControlChange(controlChange);
+  MIDI.setCallbackProgramChange(programChange);
+  MIDI.setCallbackPitchBend(pitchBend);
+  MIDI.setCallbackReset(reset);
+  YMF825.sendTone();
+  while(1){
+    MIDI.midiParse();
+  }
+}
+```
+
+拙作のMIDIライブラリと組み合わせて簡易音源にできます。
---
+++ ymf825/ymf825.cpp
@@ -0,0 +1,115 @@
+#include "ymf825.h"
+
+
+ymf825::ymf825(PinName mosi,PinName miso,PinName sck,PinName chipSelect,PinName reset) : _spi(mosi,miso,sck),_chipSelect(chipSelect),_reset(reset){
+    _spi.frequency(8000000UL);
+    init();
+}
+
+ymf825::ymf825(SPI spi,PinName chipSelect,PinName reset) : _spi(spi),_chipSelect(chipSelect),_reset(reset){
+    _spi.frequency(8000000UL);
+    init();
+}
+
+void ymf825::init(void){
+    _reset = 0;
+    wait_us(100);
+    _reset = 1;
+
+   singleWrite( 0x1D, 0x01 );
+   singleWrite( 0x02, 0x0E );
+   wait_ms(1);
+   singleWrite( 0x00, 0x01 );
+   singleWrite( 0x01, 0x00 );
+   singleWrite( 0x1A, 0xA3 );
+   wait_ms(1);
+   singleWrite( 0x1A, 0x00 );
+   wait_ms(30);
+   singleWrite( 0x02, 0x04 );
+   wait_ms(1);
+   singleWrite( 0x02, 0x00 );
+   singleWrite( 0x19, 0xF0 );
+   singleWrite( 0x1B, 0x3F );
+   singleWrite( 0x14, 0x00 );
+   singleWrite( 0x03, 0x01 );
+
+   singleWrite( 0x08, 0xF6 );
+   wait_ms(21);
+   singleWrite( 0x08, 0x00 );
+   singleWrite( 0x09, 0xF8 );
+   singleWrite( 0x0A, 0x00 );
+   
+   singleWrite( 0x17, 0x40 );
+   singleWrite( 0x18, 0x00 );
+
+    for(int i = 0;i < 16;i++){
+        singleWrite( 0x0B, i);
+        singleWrite( 0x0F, 0x30 );
+        singleWrite( 0x10, 0x71 );
+        singleWrite( 0x11, 0x00 );
+        singleWrite( 0x12, 0x08 );
+        singleWrite( 0x13, 0x00 );
+    }
+}
+
+void ymf825::singleWrite(uint8_t addr,uint8_t data){
+    _chipSelect = 0;
+    _spi.write(addr);
+    _spi.write(data);
+    _chipSelect = 1;
+    wait_us(100);
+}
+
+void ymf825::burstWrite(uint8_t addr,uint8_t *data,int length){
+    _chipSelect = 0;
+    _spi.write(addr);
+    for(int i = 0;i < length;i++){
+        _spi.write(data[i]);
+    }
+    _chipSelect = 1;
+    wait_us(100);
+}
+
+void ymf825::sendTone(void){
+   singleWrite( 0x08, 0xF6 );
+   wait_ms(1);
+   singleWrite( 0x08, 0x00 );
+    _chipSelect = 0;
+    _spi.write(0x07);
+    _spi.write(0x90);
+    for(int i = 0;i < 16;i++){
+        for(int j = 0;j <30;j++){
+            _spi.write(gmTable[toneList[i]][j]);
+        }
+    }
+    _chipSelect = 1;
+    wait_us(100);
+}
+
+void ymf825::setToneListFromGM(uint8_t ch,uint8_t num){
+    toneList[ch] = num;
+}
+
+void ymf825::noteOn(uint8_t channel,uint8_t note,uint8_t vol,uint8_t inst,unsigned short pb){
+    pitchBend(channel,pb);
+    singleWrite( 0x0B, channel );
+    singleWrite( 0x0C, 0b0001100 );
+    singleWrite( 0x0D, fNumberTableHigh[note] );
+    singleWrite( 0x0E, fNumberTableLow[note] );
+    singleWrite( 0x0F, 0x40 | (inst & 0x0F));
+}
+
+void ymf825::noteOff(uint8_t channel){
+    singleWrite( 0x0B, channel );
+    singleWrite( 0x0F, 0x00 );
+}
+
+void ymf825::pitchBend(uint8_t channel,unsigned short pb){
+    unsigned char reg[2];
+	unsigned char pit = pb/64;
+    reg[1] = (unsigned char)((tPitTbl[pit]<<1) & 0x007e);
+	reg[0] = (unsigned char)(((tPitTbl[pit]<<2) & 0x1f00)>>8);
+    singleWrite( 0x0B, channel);
+	singleWrite( 0x12, reg[0] );
+	singleWrite( 0x13, reg[1] );
+}
